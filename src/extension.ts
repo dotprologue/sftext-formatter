db1d5c8f15225e5a9944e2efa5484c552b086bdd
Ito Akira <129503198+ito-akira-dp@users.noreply.github.com>
2024-02-20 19:02:35 +0900

    First commit
    Formatter for SFText

--- src/extension.ts
+++ src/extension.ts
@@ -1,77 +1,194 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { format } from 'path';
 import { stringify } from 'querystring';
+import { start } from 'repl';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let halfWidthRegex: RegExp
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sftext-formatter" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.languages.registerDocumentFormattingEditProvider('sftext', {
-		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+	context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('sftext', {
+		provideDocumentFormattingEdits: document => {
+			//Get half width character regex from the configuration
+			const configuration = vscode.workspace.getConfiguration("SFText Formatter");
+			const halfWidthCharacterList = configuration.get("halfWidthCharacterList") as string;
+			halfWidthRegex = new RegExp(`[${halfWidthCharacterList}]`);
 			//First formatting
-			const verticalBarRegex = /^(.*?)(\|)(.*)$/;
-			const halfWidthRegex = /[\x01-\x7E\uFF65-\uFF9F]/; 
+			//Split each of the texts to the three parts
+			const verticalBarRegex = /^(.*?)(\|)(.*?)(\|)(.*)$/;
 			const textInfoArray = [...Array(document.lineCount)]
-			.map((_, index) => index)
-			.map(index => document.lineAt(index))
-			.map(t =>{
-				const matches = verticalBarRegex.exec(t.text);
-				if (matches === null) {
-					return {
-						text: t.text,
-						halfChar: 0,
-						fullChar: 0,
+				.map((_, index) => index)
+				.map(index => document.lineAt(index))
+				.map(t => t.text)
+				.map(t => {
+					const matches = verticalBarRegex.exec(t);
+					const verticalBarIndex = t.indexOf('|');
+					//Text that doesn't have any vertical bars
+					if (verticalBarIndex == -1) {
+						return {
+							scopePart: t.trim(),
+							contentPart: "",
+							commentPart: ""
+						}
 					}
-				}
-				else {
-					const trimedScopePart = matches[1].trim();
-					return {
-						text: `${trimedScopePart}${matches[2]} ${matches[3].trim()}`,
-						halfChar: [...trimedScopePart]
-						.filter(c => halfWidthRegex.test(c))
-						.length,
-						fullChar: [...trimedScopePart]
-						.filter(c => !halfWidthRegex.test(c))
-						.length,
+					//Text that have only single vertical bar
+					else if (matches === null) {
+						return {
+							scopePart: t.slice(0, verticalBarIndex).trim(),
+							contentPart: t.slice(verticalBarIndex + 1, t.length).trim(),
+							commentPart: ""
+						}
 					}
-				}
-			});
-			//Compute half-width and full-width
-			const maxHalfChar = textInfoArray
-			.map(info => info.halfChar)
-			.reduce((a, b) => Math.max(a, b)) + 1;
-			const maxFullChar = textInfoArray
-			.map(info => info.fullChar)
-			.reduce((a, b) => Math.max(a, b));
+					//Text that matches with the regex completely
+					else {
+						//Split the text to the three parts
+						return {
+							scopePart: matches[1].trim(),
+							contentPart: matches[3].trim(),
+							commentPart: matches[5].trim()
+						}
+					}
+				})
+				.map(info => {
+					return {
+						scopePart: info.scopePart,
+						scopeHalf: getHalfCharCount(info.scopePart),
+						scopeFull: getFullCharCount(info.scopePart),
+						contentPart: info.contentPart,
+						contentHalf: getHalfCharCount(info.contentPart),
+						contentFull: getFullCharCount(info.contentPart),
+						commentPart: info.commentPart
+					}
+				});
+			//Compute each of max half-width and full-width in each of scope and content part
+			const maxScopeHalf = textInfoArray
+				.map(info => info.scopeHalf)
+				.reduce((a, b) => Math.max(a, b));
+			const maxScopeFull = textInfoArray
+				.map(info => info.scopeFull)
+				.reduce((a, b) => Math.max(a, b));
+			const maxContentHalf = textInfoArray
+				.map(info => info.contentHalf)
+				.reduce((a, b) => Math.max(a, b));
+			const maxContentFull = textInfoArray
+				.map(info => info.contentFull)
+				.reduce((a, b) => Math.max(a, b));
 			//Second formatting
-			const fixedTextArray = textInfoArray
-			.map(info => {
-				const barIndex = info.text.indexOf("|");
-				if (barIndex === -1) {
-					return info.text;
-				} else {
-					const halfWhitespaces = " ".repeat(maxHalfChar - info.halfChar);
-					const fullWhiteSpaces = "　".repeat(maxFullChar - info.fullChar);
-					return `${info.text.slice(0, barIndex)}${halfWhitespaces}${fullWhiteSpaces}${info.text.slice(barIndex, info.text.length)}`
-				}
-			})
-			//Reflect results
-			return [...Array(document.lineCount)]
-			.map((_, index) => index)
-			.map(index => vscode.TextEdit.replace(document.lineAt(index).range,fixedTextArray[index]));
-		}
-	  });
+			//Unify each of the texts
+			const extraSpaceInfoArray = textInfoArray
+				.map(info => {
+					return {
+						scopeHalfSpace: maxScopeHalf - info.scopeHalf,
+						scopeFullSpace: maxScopeFull - info.scopeFull,
+						contentHalfSpace: maxContentHalf - info.contentHalf,
+						contentFullSpace: maxContentFull - info.contentFull
+					}
+				});
 
-	context.subscriptions.push(disposable);
+			//1 unit
+			const halfConstant = 20;
+			const fullConstant = 11;
+
+			const minScopeUnit = extraSpaceInfoArray
+				.map(info => quotient(info.scopeHalfSpace, halfConstant) + quotient(info.scopeFullSpace, fullConstant))
+				.reduce((a, b) => Math.min(a, b));
+			const minContentUnit = extraSpaceInfoArray
+				.map(info => quotient(info.contentHalfSpace, halfConstant) + quotient(info.contentFullSpace, fullConstant))
+				.reduce((a, b) => Math.min(a, b));
+
+			const fixedTextArray = textInfoArray
+				.map(info => {
+					let scopeRemovableUnit = minScopeUnit;
+					let scopeHalfSpace = maxScopeHalf - info.scopeHalf;
+					let scopeFullSpace = maxScopeFull - info.scopeFull;
+					let contentRemovableUnit = minContentUnit;
+					let contentHalfSpace = maxContentHalf - info.contentHalf;
+					let contentFullSpace = maxContentFull - info.contentFull;
+					//Remove full whitespaces in scope
+					const scopeFullUnit = quotient(scopeFullSpace, fullConstant);
+					if (scopeFullUnit > 0 && scopeRemovableUnit > 0) {
+						if (scopeFullUnit <= scopeRemovableUnit) {
+							scopeFullSpace -= fullConstant * scopeFullUnit;
+							scopeRemovableUnit -= scopeFullUnit;
+						}
+						else {
+							scopeFullSpace -= fullConstant * scopeRemovableUnit;
+							scopeRemovableUnit = 0;
+						}
+					}
+					//Remove half whitespaces in scope
+					const scopeHalfUnit = quotient(scopeHalfSpace, halfConstant);
+					if (scopeHalfUnit && scopeRemovableUnit > 0) {
+						if (scopeHalfUnit <= scopeRemovableUnit) {
+							scopeHalfSpace -= halfConstant * scopeHalfUnit;
+							scopeRemovableUnit -= scopeHalfUnit;
+						}
+						else {
+							scopeHalfSpace -= halfConstant * scopeRemovableUnit;
+							scopeRemovableUnit = 0;
+						}
+					}
+					//Remove full whitespaces in scope
+					const contentFullUnit = quotient(contentFullSpace, fullConstant);
+					if (contentFullUnit > 0 && contentRemovableUnit > 0) {
+						if (contentFullUnit <= contentRemovableUnit) {
+							contentFullSpace -= fullConstant * contentFullUnit;
+							contentRemovableUnit -= contentFullUnit;
+						}
+						else {
+							contentFullSpace -= fullConstant * contentRemovableUnit;
+							contentRemovableUnit = 0;
+						}
+					}
+					//Remove half whitespaces in scope
+					const contentHalfUnit = quotient(contentHalfSpace, halfConstant);
+					if (contentHalfUnit && contentRemovableUnit > 0) {
+						if (contentHalfUnit <= contentRemovableUnit) {
+							contentHalfSpace -= halfConstant * contentHalfUnit;
+							contentRemovableUnit -= contentHalfUnit;
+						}
+						else {
+							contentHalfSpace -= halfConstant * contentRemovableUnit;
+							contentRemovableUnit = 0;
+						}
+					}
+					const scopeWhite = `${"　".repeat(scopeFullSpace)}${" ".repeat(scopeHalfSpace)}`
+					const contentWhite = `${"　".repeat(contentFullSpace)}${" ".repeat(contentHalfSpace)}`;
+					return `${info.scopePart}${scopeWhite} | ${info.contentPart}${contentWhite} | ${info.commentPart}`;
+				})
+
+			return [...Array(document.lineCount)]
+				.map((_, index) => index)
+				.map(index => vscode.TextEdit.replace(document.lineAt(index).range, fixedTextArray[index]));
+		}
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('sftext-helloworld', () => {
+		vscode.window.showInformationMessage("Hello, world!");
+
+		const { activeTextEditor } = vscode.window;
+		if (activeTextEditor && activeTextEditor.document.languageId === 'sftext') {
+			const { document } = activeTextEditor;
+		}
+	}));
+}
+
+function quotient(a: number, b: number) {
+	return Math.floor(a / b);
+}
+
+function getHalfCharCount(text: string): number {
+	return [...text]
+		.filter(c => halfWidthRegex.test(c))
+		.length;
+}
+
+function getFullCharCount(text: string): number {
+	return [...text]
+		.filter(c => !halfWidthRegex.test(c))
+		.length;
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
