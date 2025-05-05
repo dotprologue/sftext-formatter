2d558fc61b66f20d1d7d8457b0e480734b227d86
Ito Akira <129503198+ito-akira-dp@users.noreply.github.com>
2025-05-06 00:26:25 +0900

    Update v.1.1.0

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # SFText Formatter
 
-Formatter for SFText. SFText is a script format used in ScenarioFlow.
+This extension provides a formatter for SFText in ScenarioFlow. See [here](https://github.com/dotprologue/ScenarioFlow.git) for details about ScenarioFlow and the usage of this extension.
 
-See [here](https://github.com/dotprologue/ScenarioFlow.git) for details about ScenarioFlow.
-
-![example](./images/example.gif)
+| Settings | Summary |
+| --- | --- |
+| Half Width Character List | Enter characters to be recognized as half-width in the regular expression manner |
+| Half Width Constant | Enter the number of half-width characters required for the alignment |
+| Full Width Constant | Enter the number of full-width characters required for the alignment |
--- src/extension.ts
+++ src/extension.ts
@@ -1,193 +1,139 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { format } from 'path';
+import { error } from 'console';
+import { format, parse } from 'path';
+import { config } from 'process';
 import { stringify } from 'querystring';
 import { start } from 'repl';
+import internal = require('stream');
+import { text } from 'stream/consumers';
 import * as vscode from 'vscode';
 
-let halfWidthRegex: RegExp
+type LineElementLength = {
+	blockCount: number
+	halfCharRemainder: number,
+	fullCharRemainder: number
+};
+
+type LineElement = {
+	text: string,
+	length: LineElementLength
+};
+
+type ParsedLine = {
+	annotationPart: LineElement,
+	declaratorPart: LineElement,
+	contentPart: LineElement,
+	comment: string
+};
+
+function getMaxLineLength(lineLengths: Array<LineElementLength>): LineElementLength {
+	return lineLengths.reduce((acc, val) => {
+		return {
+			blockCount: Math.max(acc.blockCount, val.blockCount),
+			halfCharRemainder: Math.max(acc.halfCharRemainder, val.halfCharRemainder),
+			fullCharRemainder: Math.max(acc.fullCharRemainder, val.fullCharRemainder)
+		};
+	}, {
+		blockCount: 0,
+		halfCharRemainder: 0,
+		fullCharRemainder: 0
+	});
+}
+
+function subtract(a: LineElementLength, b: LineElementLength): LineElementLength {
+	return {
+		blockCount: a.blockCount - b.blockCount,
+		halfCharRemainder: a.halfCharRemainder - b.halfCharRemainder,
+		fullCharRemainder: a.fullCharRemainder - b.fullCharRemainder
+	};
+}
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('sftext', {
 		provideDocumentFormattingEdits: document => {
-			//Get half width character regex from the configuration
+			// Determine what characters are half width
 			const configuration = vscode.workspace.getConfiguration("SFText Formatter");
 			const halfWidthCharacterList = configuration.get("halfWidthCharacterList") as string;
-			halfWidthRegex = new RegExp(`[${halfWidthCharacterList}]`);
-			//First formatting
-			//Split each of the texts to the three parts
-			const verticalBarRegex = /^(.*?)(\|)(.*?)(\|)(.*)$/;
-			const textInfoArray = [...Array(document.lineCount)]
-				.map((_, index) => index)
-				.map(index => document.lineAt(index))
-				.map(t => t.text)
-				.map(t => {
-					const matches = verticalBarRegex.exec(t);
-					const verticalBarIndex = t.indexOf('|');
-					//Text that doesn't have any vertical bars
-					if (verticalBarIndex == -1) {
-						return {
-							scopePart: t.trim(),
-							contentPart: "",
-							commentPart: ""
-						}
-					}
-					//Text that have only single vertical bar
-					else if (matches === null) {
-						return {
-							scopePart: t.slice(0, verticalBarIndex).trim(),
-							contentPart: t.slice(verticalBarIndex + 1, t.length).trim(),
-							commentPart: ""
-						}
-					}
-					//Text that matches with the regex completely
-					else {
-						//Split the text to the three parts
-						return {
-							scopePart: matches[1].trim(),
-							contentPart: matches[3].trim(),
-							commentPart: matches[5].trim()
-						}
-					}
-				})
-				.map(info => {
-					return {
-						scopePart: info.scopePart,
-						scopeHalf: getHalfCharCount(info.scopePart),
-						scopeFull: getFullCharCount(info.scopePart),
-						contentPart: info.contentPart,
-						contentHalf: getHalfCharCount(info.contentPart),
-						contentFull: getFullCharCount(info.contentPart),
-						commentPart: info.commentPart
-					}
+			const halfWidthConstant = configuration.get("halfWidthConstant") as number;
+			const fullWidthConstant = configuration.get("fullWidthConstant") as number;
+
+			const halfWidthRegex = new RegExp(`[${halfWidthCharacterList}]`);
+			const emptyLineElement: LineElement = {
+				text: "",
+				length: {
+					blockCount: 0,
+					halfCharRemainder: 0,
+					fullCharRemainder: 0
+				}
+			};
+			// Parse each line
+			const splitLines: Array<Array<string>> = [...Array(document.lineCount)]
+				.map((_, index) => document.lineAt(index).text)
+				.map(t => t.split('|'));
+			const annotationExists = splitLines.some(elems => elems.length > 3);
+			const commentBoundary = annotationExists ? 3 : 2;
+			const parsedLines: Array<ParsedLine> = splitLines
+				.map(elems => elems.map((e, i) => i < commentBoundary ? e.trim() : e))
+				.map(elems => {
+					const length = elems.length;
+					const parsedText: ParsedLine = annotationExists ? {
+						annotationPart: length > 0 ? getLineElement(elems[0], halfWidthRegex) : emptyLineElement,
+						declaratorPart: length > 1 ? getLineElement(elems[1], halfWidthRegex) : emptyLineElement,
+						contentPart: length > 2 ? getLineElement(elems[2], halfWidthRegex) : emptyLineElement,
+						comment: length > 3 ? elems.slice(3, length).reduce((prev, curr, index) => index === 0 ? curr : `${prev}|${curr}`, "").trim() : ""
+					} : {
+						annotationPart: emptyLineElement,
+						declaratorPart: length > 0 ? getLineElement(elems[0], halfWidthRegex) : emptyLineElement,
+						contentPart: length > 1 ? getLineElement(elems[1], halfWidthRegex) : emptyLineElement,
+						comment: length > 2 ? elems.slice(2, length).reduce((prev, curr, index) => index === 0 ? curr : `${prev}|${curr}`, "").trim() : ""
+					};
+					return parsedText;
 				});
-			//Compute each of max half-width and full-width in each of scope and content part
-			const maxScopeHalf = textInfoArray
-				.map(info => info.scopeHalf)
-				.reduce((a, b) => Math.max(a, b));
-			const maxScopeFull = textInfoArray
-				.map(info => info.scopeFull)
-				.reduce((a, b) => Math.max(a, b));
-			const maxContentHalf = textInfoArray
-				.map(info => info.contentHalf)
-				.reduce((a, b) => Math.max(a, b));
-			const maxContentFull = textInfoArray
-				.map(info => info.contentFull)
-				.reduce((a, b) => Math.max(a, b));
-			//Second formatting
-			//Unify each of the texts
-			const extraSpaceInfoArray = textInfoArray
-				.map(info => {
-					return {
-						scopeHalfSpace: maxScopeHalf - info.scopeHalf,
-						scopeFullSpace: maxScopeFull - info.scopeFull,
-						contentHalfSpace: maxContentHalf - info.contentHalf,
-						contentFullSpace: maxContentFull - info.contentFull
-					}
+			// Get the maximum length of every line element
+			const maxAnnotationLineLength = getMaxLineLength(parsedLines.map(line => line.annotationPart.length));
+			const maxDeclaratorLineLength = getMaxLineLength(parsedLines.map(line => line.declaratorPart.length));
+			const maxContentLineLength = getMaxLineLength(parsedLines.map(line => line.contentPart.length));
+			// Align lines
+			const alignedLines: Array<string> = parsedLines
+				.map(line => {
+					const annotationDiff = subtract(maxAnnotationLineLength, line.annotationPart.length);
+					const declaratorDiff = subtract(maxDeclaratorLineLength, line.declaratorPart.length);
+					const contentDiff = subtract(maxContentLineLength, line.contentPart.length);
+					const alignedAnnotation = alignText(line.annotationPart.text, annotationDiff);
+					const alignedDeclarator = alignText(line.declaratorPart.text, declaratorDiff);
+					const alignedContent = alignText(line.contentPart.text, contentDiff);
+					const alignedLine = annotationExists ?
+						`${alignedAnnotation} | ${alignedDeclarator} | ${alignedContent} | ${line.comment}` :
+						`${alignedDeclarator} | ${alignedContent} | ${line.comment}`;
+					return alignedLine;
 				});
-
-			//1 unit
-			const halfConstant = 20;
-			const fullConstant = 11;
-
-			const minScopeUnit = extraSpaceInfoArray
-				.map(info => quotient(info.scopeHalfSpace, halfConstant) + quotient(info.scopeFullSpace, fullConstant))
-				.reduce((a, b) => Math.min(a, b));
-			const minContentUnit = extraSpaceInfoArray
-				.map(info => quotient(info.contentHalfSpace, halfConstant) + quotient(info.contentFullSpace, fullConstant))
-				.reduce((a, b) => Math.min(a, b));
-
-			const fixedTextArray = textInfoArray
-				.map(info => {
-					let scopeRemovableUnit = minScopeUnit;
-					let scopeHalfSpace = maxScopeHalf - info.scopeHalf;
-					let scopeFullSpace = maxScopeFull - info.scopeFull;
-					let contentRemovableUnit = minContentUnit;
-					let contentHalfSpace = maxContentHalf - info.contentHalf;
-					let contentFullSpace = maxContentFull - info.contentFull;
-					//Remove full whitespaces in scope
-					const scopeFullUnit = quotient(scopeFullSpace, fullConstant);
-					if (scopeFullUnit > 0 && scopeRemovableUnit > 0) {
-						if (scopeFullUnit <= scopeRemovableUnit) {
-							scopeFullSpace -= fullConstant * scopeFullUnit;
-							scopeRemovableUnit -= scopeFullUnit;
-						}
-						else {
-							scopeFullSpace -= fullConstant * scopeRemovableUnit;
-							scopeRemovableUnit = 0;
-						}
-					}
-					//Remove half whitespaces in scope
-					const scopeHalfUnit = quotient(scopeHalfSpace, halfConstant);
-					if (scopeHalfUnit && scopeRemovableUnit > 0) {
-						if (scopeHalfUnit <= scopeRemovableUnit) {
-							scopeHalfSpace -= halfConstant * scopeHalfUnit;
-							scopeRemovableUnit -= scopeHalfUnit;
-						}
-						else {
-							scopeHalfSpace -= halfConstant * scopeRemovableUnit;
-							scopeRemovableUnit = 0;
-						}
-					}
-					//Remove full whitespaces in scope
-					const contentFullUnit = quotient(contentFullSpace, fullConstant);
-					if (contentFullUnit > 0 && contentRemovableUnit > 0) {
-						if (contentFullUnit <= contentRemovableUnit) {
-							contentFullSpace -= fullConstant * contentFullUnit;
-							contentRemovableUnit -= contentFullUnit;
-						}
-						else {
-							contentFullSpace -= fullConstant * contentRemovableUnit;
-							contentRemovableUnit = 0;
-						}
-					}
-					//Remove half whitespaces in scope
-					const contentHalfUnit = quotient(contentHalfSpace, halfConstant);
-					if (contentHalfUnit && contentRemovableUnit > 0) {
-						if (contentHalfUnit <= contentRemovableUnit) {
-							contentHalfSpace -= halfConstant * contentHalfUnit;
-							contentRemovableUnit -= contentHalfUnit;
-						}
-						else {
-							contentHalfSpace -= halfConstant * contentRemovableUnit;
-							contentRemovableUnit = 0;
-						}
-					}
-					const scopeWhite = `${"　".repeat(scopeFullSpace)}${" ".repeat(scopeHalfSpace)}`
-					const contentWhite = `${"　".repeat(contentFullSpace)}${" ".repeat(contentHalfSpace)}`;
-					return `${info.scopePart}${scopeWhite} | ${info.contentPart}${contentWhite} | ${info.commentPart}`;
-				})
-
 			return [...Array(document.lineCount)]
 				.map((_, index) => index)
-				.map(index => vscode.TextEdit.replace(document.lineAt(index).range, fixedTextArray[index]));
+				.filter(index => document.lineAt(index).text !== alignedLines[index])
+				.map(index => vscode.TextEdit.replace(document.lineAt(index).range, alignedLines[index]));
+
+			function getLineElement(text: string, halfWidthRegex: RegExp): LineElement {
+				const classifiedChars = [...text].map(c => halfWidthRegex.test(c));
+				const halfCharCount = classifiedChars.filter(isHalf => isHalf).length;
+				const fullCharCount = classifiedChars.filter(isHalf => !isHalf).length;
+				return {
+					text: text,
+					length: {
+						blockCount: Math.floor(halfCharCount / halfWidthConstant) + Math.floor(fullCharCount / fullWidthConstant),
+						halfCharRemainder: halfCharCount % halfWidthConstant,
+						fullCharRemainder: fullCharCount % fullWidthConstant
+					}
+				};
+			}
+
+			function alignText(text: string, diffLength: LineElementLength): string {
+				const halfWhitespaces = " ".repeat(halfWidthConstant * diffLength.blockCount + diffLength.halfCharRemainder);
+				const fullWhitespaces = "　".repeat(diffLength.fullCharRemainder);
+				return `${text}${halfWhitespaces}${fullWhitespaces}`;
+			}
 		}
 	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('sftext-helloworld', () => {
-		vscode.window.showInformationMessage("Hello, world!");
-
-		const { activeTextEditor } = vscode.window;
-		if (activeTextEditor && activeTextEditor.document.languageId === 'sftext') {
-			const { document } = activeTextEditor;
-		}
-	}));
-}
-
-function quotient(a: number, b: number) {
-	return Math.floor(a / b);
-}
-
-function getHalfCharCount(text: string): number {
-	return [...text]
-		.filter(c => halfWidthRegex.test(c))
-		.length;
-}
-
-function getFullCharCount(text: string): number {
-	return [...text]
-		.filter(c => !halfWidthRegex.test(c))
-		.length;
 }
 
 // This method is called when your extension is deactivated
